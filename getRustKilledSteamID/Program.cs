da942ab58c595922809cb27d081b00cfb035674d
Morgyn <Morgyn@gmail.com>
2022-10-03 16:59:48 +0100

    Add project files.

---
+++ getRustKilledSteamID/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace getRustKilledSteamID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey rustKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 252490", false);
+            if (rustKey == null)
+            {
+                Console.WriteLine("Couldn't open registry");
+                return;
+            }
+            
+            String path = (String)rustKey.GetValue("InstallLocation");
+            Console.WriteLine("Rust Path: {0}", path);
+
+            using (StreamReader sr = File.OpenText(path + "/output_log.txt"))
+            {
+                string s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (s.StartsWith("You died: killed by") )
+                        Console.WriteLine(s);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
